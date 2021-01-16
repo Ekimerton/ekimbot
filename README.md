e44a55142214da6c9a390097b9ce52d882aa8391
Ekimerton <ekim0252@gmail.com>
2021-01-16 18:44:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A discord I made for random stuff down the road. Right now, it is able to make teams of _ from a given voice channel.
---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+import math
+import numpy as np
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('@ekimbot'):
+        await message.channel.send("I'm down")
+        return 
+
+    if message.content.startswith('$team'):
+        team_size = -1
+        total_size = -1
+
+        try:
+            args = message.content.split(" ")
+            team_size = int(args[1])
+        except Exception as e:
+            await message.channel.send(e)
+            return
+
+        users = []
+        try:
+            author = message.author
+            voice_channel = message.author.voice.channel
+            for member in voice_channel.voice_states:
+                user = await client.fetch_user(member)
+                users.append(user.name)
+            if 'Groovy' in users:
+                users.remove('Groovy')
+            total_size = len(users)
+        except Exception as e:
+            await message.channel.send(e)
+            return
+
+        perm = np.random.permutation(users)
+        teams = np.array_split(perm, math.ceil(total_size / team_size))
+
+        result = []
+        for team in teams:
+            team = [str(i) for i in team]
+            result.append('(' + ', '.join(team) + ')')
+
+        await message.channel.send(', '.join(result))
+
+    if message.content.startswith('$help'):
+        await message.channel.send('Use $team x to generate teams of x')
+
+client.run('Nzk5NDc3NDUxMjMwMTUwNjg2.YAEJZQ.mlGCt0uvUqTukRLaIiXzagJYsKs')
