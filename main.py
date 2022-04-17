68509dd2f64c1fbd8102e000a68ec8676e406101
Ekimerton <ekim0252@gmail.com>
2022-04-17 03:41:06 -0400

    Added wordle leaderboard basics

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import random
 import re
 import numpy as np
 import os
+import bitdotio
+
+DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+BITIO_TOKEN = os.environ.get('BITIO_TOKEN')
+
 
 client = discord.Client()
+b = bitdotio.bitdotio(BITIO_TOKEN)
 
 
 @client.event
@@ -18,7 +24,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    banned_words = ['league of legends', 'league', 'ieague', 'dota', 'dota 2', 'aram', 'tft']
+    banned_words = ['league of legends', 'league',
+                    'ieague', 'dota', 'dota 2', 'aram', 'tft']
     regex = re.compile('|'.join(map(re.escape, banned_words)), re.IGNORECASE)
     if any(word in message.content.lower() for word in banned_words):
         await message.channel.send(":warning: Fatwa placed on " + message.author.mention + " for the following message :warning: : \n\n" + regex.sub("`REDACTED`", message.content) + '\n')
@@ -45,7 +52,6 @@ async def on_message(message):
 
         users = []
         try:
-            author = message.author
             voice_channel = message.author.voice.channel
             for member in voice_channel.voice_states:
                 user = await client.fetch_user(member)
@@ -75,5 +81,51 @@ async def on_message(message):
             '''
         )
 
-discord_token = os.environ.get('DISCORD_TOKEN')
-client.run(discord_token)
+    if message.content.startswith('$wordleboard'):
+        conn = b.get_connection()
+        cur = conn.cursor()
+
+        cur.execute(
+            'SELECT max("wordle_number") from "Ekimerton/ekimbot"."wordle"')
+        latest_wordle = cur.fetchone()[0]
+
+        cur.execute(
+            'SELECT min(wordle_in) FROM "Ekimerton/ekimbot"."wordle" WHERE "wordle_number" = {latest_wordle}'.format(latest_wordle=latest_wordle))
+        latest_best = cur.fetchone()[0]
+
+        cur.execute(
+            'SELECT DISTINCT user_id FROM "Ekimerton/ekimbot"."wordle" WHERE wordle_number = {latest_wordle} AND wordle_in = {latest_best}'.format(latest_wordle=latest_wordle, latest_best=latest_best))
+        latest_winners = cur.fetchall()
+        winners = [winner[0] for winner in latest_winners]
+
+        winner_text = "Current winner(s) for wordle {latest_wordle} with a score of {latest_best}/6: \n\n".format(
+            latest_wordle=latest_wordle, latest_best=latest_best)
+        for winner in winners:
+            user = await client.fetch_user(winner)
+            winner_text += user.mention + '\n'
+        await message.channel.send(winner_text)
+
+    if message.content.startswith('Wordle'):
+        user_id = message.author.id
+        wordle_number = message.content.split(" ")[1]
+        wordle_in = message.content.split(" ")[2][0]
+        hard_mode = message.content.split(" ")[2][3] == '*'
+
+        conn = b.get_connection()
+        cur = conn.cursor()
+        try:
+            cur.execute('INSERT INTO "Ekimerton/ekimbot"."wordle" ("user_id", "wordle_number", "wordle_in", "hard_mode") VALUES ({user_id}, {wordle_number}, {wordle_in}, {hard_mode})'.format(
+                user_id=user_id, wordle_number=wordle_number, wordle_in=wordle_in, hard_mode=hard_mode))
+            await message.add_reaction('✅')
+        except:
+            await message.add_reaction('❌')
+        await message.channel.send(
+            '''
+User ID: {id} \n
+Worldle Number: {num} \n
+Wordle In: {wordle_in} \n
+Hard Mode: {hard} \n
+            '''.format(id=user_id, num=wordle_number, wordle_in=wordle_in, hard=hard_mode)
+        )
+
+client.run(DISCORD_TOKEN)
