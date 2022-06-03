3c51c9d7e8e3a1487ef6729326583fce4ed8fdbf
Ekimerton <ekim0252@gmail.com>
2022-06-03 00:22:24 -0400

    Fixed single quote error

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,10 @@ def wordle_to_char(wordle_input):
     wordle_input = wordle_input.replace('🟨', '1')
     wordle_input = wordle_input.replace('🟩', '2')
     base_3 = int(wordle_input, 3)
-    return chr(base_3 + 20)
+    char = chr(base_3 + 20)
+    if char == "'":
+        char = "\'"
+    return char
 
 
 def char_to_wordle(char):
