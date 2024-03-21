cf66a68542d7ec4a9f5ee0d04f11ee6346156d11
Ekimerton <ekim0252@gmail.com>
2024-03-21 18:39:09 -0400

    Added ban

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from commands import coinflip, team, ban
 # Initialize Intents
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
 client = discord.Client(intents=intents)
@@ -30,5 +31,18 @@ async def on_message(message):
 
     if message.content.startswith('$ban'):
         await ban.run_command(message)
+
+    if message.content.startswith('$help'):
+        help_message = """
+Here are the commands you can use:
+
+$coinflip - Flips a coin and returns either heads or tails.
+
+$team [TEAM_SIZE] - Randomly divides the people in your voice channel into teams of the specified size. You need to be in a voice channel to use this.
+
+$ban - Randomly selects a member from the server (excluding bots) and sends their name as a chosen one.
+"""
+        await message.channel.send(help_message)
+
 
 client.run(DISCORD_TOKEN)
--- commands/ban.py
+++ commands/ban.py
@@ -4,17 +4,20 @@ import discord
 
 async def run_command(message: discord.Message):
     try:
-        members = message.guild.members
-        # Exclude bots
-        users = [member.name for member in members if not member.bot]
-        if not users:
+        # Make sure your bot has the 'members' intent enabled to use this
+        members = []
+        async for member in message.guild.fetch_members(limit=None):
+            if not member.bot:  # Exclude bots
+                members.append(member.name)
+
+        if not members:
             await message.channel.send("No members found.")
             return
 
         # Choose a random user
-        random_user = random.choice(users)
+        random_user = random.choice(members)
 
         # Send the chosen user's name
         await message.channel.send(f"{random_user} has been chosen.")
     except Exception as e:
-        await message.channel.send("An error occurred while attempting to select a random member.")
+        await message.channel.send(e)
